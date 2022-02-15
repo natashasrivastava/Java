2ba7f8719c7b021fa0007b91828262e555bfed4f
natashasrivastava <srivastavanatasha24@gmail.com>
2022-02-15 12:04:37 +0530

    Notes

---
+++ Practice/src/Basics/OptionalTest.java
@@ -0,0 +1,19 @@
+package Basics;
+
+public class Testrun {
+    public static void main(String[] args) {
+      try{
+          int x=0;
+          for(x=1;x<4;x++);
+              System.out.println(x);
+      }
+      catch (Exception e){
+
+      }
+      finally {
+          System.out.println("error");
+      }
+    }
+
+}
+
